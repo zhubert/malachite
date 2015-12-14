902469488b15f18e95787b02bd931b1f6da13645
Zack Hubert <zack@ministrycentered.com>
2015-12-14 10:40:37 -0800

    fix(header files too)

--- lib/emerald/rails.rb
+++ lib/emerald/rails.rb
@@ -9,6 +9,9 @@ module Emerald
       Dir.glob(Rails.root.join('tmp', '*.so')).each do |file|
         File.delete(file)
       end
+      Dir.glob(Rails.root.join('tmp', '*.h')).each do |file|
+        File.delete(file)
+      end
     end
   end
 end
