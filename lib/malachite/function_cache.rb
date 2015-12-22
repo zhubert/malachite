670b5743b2b994b13e4c5f124ad4ddc6dc9e1f98
Zack Hubert <zack@ministrycentered.com>
2015-12-22 11:18:43 -0800

    chore(self all the things)

--- lib/malachite/function_cache.rb
+++ lib/malachite/function_cache.rb
@@ -2,7 +2,7 @@ module Malachite
   def self.function_cache
     @function_cache ||= {}
   end
-  
+
   def self.from_function_cache(method_name)
     existing_function = Malachite.function_cache[method_name]
     return existing_function if existing_function.present?
@@ -11,32 +11,27 @@ module Malachite
 
   def self.add_to_function_cache(method_name)
     @function_cache ||= {}
-    function = fiddle_function(method_name)
+    function = Malachite.fiddle_function(method_name)
     @function_cache[method_name] = function
     function
   end
 
   def self.dylib
-    @dylib ||= open_dylib
-  end
-
-
-  def fiddle_function(method_name)
-    Fiddle::Function.new(Malachite.dylib[called_method(method_name)], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_VOIDP)
-  end
-
-  def called_method(method_name)
-    "call#{method_name.to_s.camelize}"
-  end
-
-  def open_dylib
-    Fiddle.dlopen(shared_object_path)
+    @dylib ||= Fiddle.dlopen(Malachite.shared_object_path)
   rescue Fiddle::DLError
     raise Malachite::DLError, 'Unable to open dynamic library.'
   end
 
-  def shared_object_path
-    Malachite::Compiler.new.compile
+  def self.fiddle_function(method_name)
+    call_method = Malachite.called_method(method_name)
+    Fiddle::Function.new(Malachite.dylib[call_method], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_VOIDP)
   end
 
+  def self.called_method(method_name)
+    "call#{method_name.to_s.camelize}"
+  end
+
+  def self.shared_object_path
+    @so_path ||= Malachite::Compiler.new.compile
+  end
 end
